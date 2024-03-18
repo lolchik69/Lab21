bd6b84465a8880f579bd151db62a27aebdc3cf7f
DIANA <golubovskaya00@inbox.ru>
2024-03-18 18:13:46 +0300

    added enter_a func

--- Program.cs
+++ Program.cs
@@ -6,4 +6,12 @@ class Program {
 
     public static void Main(string[] args) {
     } 
+    public static void EnterA() {
+        Console.WriteLine("Enter a: ");
+        string str_a = Console.ReadLine()!;
+        while (!double.TryParse(str_a, out a)) {
+            Console.WriteLine("Try again");
+            str_a = Console.ReadLine()!;
+        }
+    }
 }
