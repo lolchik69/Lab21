4e29f0504d8a5520abe89054c3075a86f29f6107
DIANA <golubovskaya00@inbox.ru>
2024-03-18 18:22:32 +0300

    added div_op func

--- Program.cs
+++ Program.cs
@@ -27,5 +27,11 @@ class Program {
 
     public static double Multiply(double a, double b) => a * b;
     
+    public static double Divide(double a, double b) {
+        if (b == 0) 
+            throw new DivideByZeroException("can't divide by zero");
+
+        return a / b;
+    }
 
 }
